5ed86c5eaf2adc38f11df47f80bc5b6162656a6d
Alessandro Canova <alessandrocanova14@gmail.com>
2021-01-22 19:19:47 +0100

    re-install modules

--- api/app.js
+++ api/app.js
@@ -5,12 +5,16 @@ const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes');
 const { sequelize } = require('./models');
+const cors = require('cors');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
 // create the Express app
 const app = express();
+
+// Enable all CORS Requests
+app.use(cors());
 
 // Setup request body JSON parsing.
 app.use(express.json());
