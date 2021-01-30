f433cf74703236e273a6fa97d06377f47f38f0f3
Alessandro Canova <alessandrocanova14@gmail.com>
2021-01-31 00:16:08 +0100

    Create CourseDisplay.js

---
+++ client/src/components/CourseDisplay.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+
+const CourseDisplay = ({title, id}) => {
+
+    return (
+        <div className="bounds">
+            <div className="grid-33">
+                <Link to={`/courses/${id}`} className="course--module course--link">
+                    <h4 className="course--label">Course</h4>
+                    <h3 className="course--title">{title}</h3>
+                </Link>
+            </div>
+        </div>
+    );
+
+}
+
+
+
+
+export default CourseDisplay;
+
