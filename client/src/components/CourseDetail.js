7f48c6b88d1b27e2ee65893b759bbafc12e442f5
Alessandro Canova <alessandrocanova14@gmail.com>
2021-02-10 22:18:07 +0100

    render CourseDetail

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -25,4 +25,93 @@ export default class CourseDetail extends Component {
             console.log(error);
         }
     }
+
+    // DELETE request to the REST API's /api/courses/:id
+    deleteCourse = (id) => {
+
+        // gives access to the data in Data.js and actions in Context.js
+        const { context } = this.props;
+        const inputConfirmed = window.confirm('Are you sure you want to delete this course?');
+        
+        if(inputConfirmed) {
+            context.data.deleteCourse(id)
+                .then(data => {
+                    if(data) { 
+                        // if deleted return at the home page
+                        this.props.history.push('/');
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+
+    };
+
+    render () {
+
+        const { course } = this.state;
+        const { user } = course;
+
+        let editButtons;
+
+        editButtons = (
+            <div className="actions--bar">
+                <div className="bounds">
+                    <div className="grid-100">
+                        <button className="button" onClick={() => this.props.history.push(`/courses/${course.id}/update`)}>
+                            Update Course
+                        </button>
+                        <button className="button" onClick={() => this.deleteCourse(course.id)}>
+                            Delete Course
+                        </button>
+                        <button className="button button-secondary" onClick={() => this.props.history.push('/')}>
+                            Return to List
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+
+        return (
+
+            <>
+                {editButtons}
+
+                <div className="bounds course--detail">
+                    <div class="grid-66">
+                        <div class="course--header">
+                            <h4 className="course--label">Course</h4>
+                            <h3 className="course--title">{course.title}</h3>
+                            <p>By </p>
+                        </div>
+                        <div className="course--description">{course.description}</div>
+                    </div>
+                    <div class="grid-25 grid-right">
+                        <div class="course--stats">
+                            <ul class="course--stats--list">
+                                <li class="course--stats--list--item">
+                                    <h4>Estimated Time</h4>
+                                    <h3>{course.estimatedTime}</h3>
+                                </li>
+                                <li class="course--stats--list--item">
+                                    <h4>Materials Needed</h4>
+                                    <ul>
+                                        <li>{course.materialsNeeded}</li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </>
+
+        );
+
+
+
+    }
+
+    
+
 }
