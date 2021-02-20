fd4e27a7f67489edcaec1458d0eda3f7e3c4a1ae
Alessandro Canova <alessandrocanova14@gmail.com>
2021-02-20 17:24:25 +0100

    render UserSignIn

--- client/src/App.js
+++ client/src/App.js
@@ -8,13 +8,14 @@ import {
 // Import all components
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
+import UserSignIn from './components/UserSignIn';
 
 // Use withContext with all its components.
 import withContext from './Context';
 
 const CoursesWithContext = withContext(Courses);
 const CourseDetailWithContext = withContext(CourseDetail);
-
+const UserSignInWithContext = withContext(UserSignIn);
 
 
 export default () => (
@@ -24,6 +25,7 @@ export default () => (
       <Switch>
         <Route exact path= "/" component={CoursesWithContext} />
         <Route path= "/courses/:id" component={CourseDetailWithContext} />
+        <Route path= "/signin" component={UserSignInWithContext} />
       </Switch>
     </main>
   </BrowserRouter>
