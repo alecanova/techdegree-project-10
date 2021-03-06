29a35bfa6e6d32930b04021777fc92bcbfb3ee5e
Alessandro Canova <alessandrocanova14@gmail.com>
2021-03-06 22:35:28 +0100

    create UserSignOut

---
+++ client/src/components/UserSignOut.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
+
+
+export default ({context}) => {
+    
+  // component calls signOut and updates state after render
+  useEffect(() =>  context.actions.signOut());
+
+
+  return (
+    <Redirect to="/" />
+  );
+
+}
