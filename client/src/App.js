72227176eeec523ee4871ba18f7e765e0d9419a9
Alessandro Canova <alessandrocanova14@gmail.com>
2021-03-20 21:11:48 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,21 +5,26 @@ import {
   Switch
 } from 'react-router-dom';
 
+
 // Import all components
+import Header from './components/Header';
 import Courses from './components/Courses';
 import CourseDetail from './components/CourseDetail';
 import UserSignIn from './components/UserSignIn';
 import UserSignUp from './components/UserSignUp';
+import UserSignOut from './components/UserSignOut';
 import CreateCourse from './components/CreateCourse';
 import UpdateCourse from './components/UpdateCourse';
 
 // Use withContext with all its components.
 import withContext from './Context';
 
+const HeaderWithContext = withContext(Header);
 const CoursesWithContext = withContext(Courses);
 const CourseDetailWithContext = withContext(CourseDetail);
 const UserSignInWithContext = withContext(UserSignIn);
 const UserSignUpWithContext = withContext(UserSignUp);
+const UserSignOutWithContext = withContext(UserSignOut);
 const CreateCourseWithContext = withContext(CreateCourse);
 const UpdateCourseWithContext = withContext(UpdateCourse);
 
@@ -28,11 +33,14 @@ export default () => (
     
   <BrowserRouter>
     <main>
+      <HeaderWithContext />
+
       <Switch>
         <Route exact path= "/" component={CoursesWithContext} />
         <Route path= "/courses/:id" component={CourseDetailWithContext} />
         <Route path= "/signin" component={UserSignInWithContext} />
         <Route path= "/signup" component={UserSignUpWithContext} />
+        <Route path= "/signout" component={UserSignOutWithContext} />
         <Route path= "/create" component={CreateCourseWithContext} />
         <Route path= {`/courses/:id/update`} component={UpdateCourseWithContext} />
       </Switch>
